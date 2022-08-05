2e179c8136fe29f08f7a484d47ddf36c1ca957a8
AliceTHE <a.d.kireeva@gmail.com>
2022-08-05 18:16:06 +0300

    task complete

--- src/index.js
+++ src/index.js
@@ -38,7 +38,17 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let arr = expr.match(/.{10}/g).map(item => item.replace(/00/g, '').replace(/10/g, '.').replace(/11/g, '-'));
+    let res = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === '**********') res.push(' ');
+        for (let j = 0; j < Object.keys(MORSE_TABLE).length; j++) {
+            if (arr[i] === Object.keys(MORSE_TABLE)[j]) {
+                res.push(Object.values(MORSE_TABLE)[j]);
+            }
+        }
+    }
+    return res.join('');
 }
 
 module.exports = {
